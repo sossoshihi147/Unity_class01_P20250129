67dc42b9dd7e32b81dfc2da30f9c08ca0e21b7c2
sossoshihi147 <sossoshihi147@gmail.com>
2025-02-05 09:08:13 +0800

    Update 作業3、4

---
+++ Assets/practise/homework_03.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+
+public class homework_03 : MonoBehaviour
+{
+    private void Awake()
+    {
+        equation(6 / 2 * (1 + 2));
+    }
+
+    private void equation(int answer)
+    {
+        Debug.Log($"<color=#ff0> 6/2(1+2)= {answer} </color>");
+    }
+}
---
+++ Assets/practise/homework_04.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+
+public class homework_04 : MonoBehaviour
+{
+    private void Awake()
+    {
+        Debug.Log($"<color=#8f0>{income(30000)}</color>");
+        Debug.Log($"<color=#f00>{surplus(30000, 32000)}</color>");
+    }
+    /// <summary>
+    /// 當月收入
+    /// </summary>
+    /// <param name="salary">薪水收入</param>
+    /// <returns></returns>
+    private  bool income(int salary )
+    {
+        Debug.Log($"<color=#0fff>這個月有收入嗎?</color>");
+        return salary > 0;
+    }
+    /// <summary>
+    /// 當月結餘
+    /// </summary>
+    /// <param name="salary">薪水收入</param>
+    /// <param name="expense">費用支出</param>
+    /// <returns></returns>
+    private bool surplus(int salary , int expense)
+    {
+        Debug.Log($"<color=#0fff>這個月有結餘嗎?</color>");
+        return (salary - expense) > 0;
+    }
+}
