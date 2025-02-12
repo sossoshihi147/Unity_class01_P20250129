eaeb12e580093122cf4ba3efc5f5e2210d3a20b3
sossoshihi147 <sossoshihi147@gmail.com>
2025-02-12 19:47:15 +0800

    Updata 認識靜態與非靜態成員差異

---
+++ Assets/Scripts/Class_07_2_GetSetStaticData.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+
+namespace OLIVER
+{
+    /// <summary>
+    /// 練習取得與設定靜態資料
+    /// </summary>
+    public class Class_07_2_GetSetStaticData : MonoBehaviour
+    {
+        // 取得非靜態資料 (成員)
+        // 1. 先定義要獲得資料類別的變數
+        // 2. 透過變數取得非靜態資料 (成員)
+        public Class_07_2_Static class_7_2;
+
+        private void Awake()
+        {
+            // 取得非靜態變數
+            // 變數名稱.非靜態成員
+            Debug.Log($"<color=#f3d>非靜態成員 : {class_7_2.inventoryWater}</color>");
+            // 取得非靜態屬性
+            Debug.Log($"<color=#f3d>非靜態成員 : {class_7_2.skillMain}</color>");
+            // 呼叫非靜態方法
+            class_7_2.Punch();
+
+            // 取得靜態變數
+            // 類別名稱.靜態成員
+            Debug.Log($"<color=#f3d>非靜態成員 : {Class_07_2_Static.inventoryProp}</color>");
+            // 取得靜態屬性
+            Debug.Log($"<color=#f3d>非靜態成員 : {Class_07_2_Static.skillSecond}</color>");
+            // 呼叫靜態方法
+            Class_07_2_Static.Kick();
+        }
+    }
+
+}
--- Assets/Scripts/Class_07_2_Static.cs
+++ Assets/Scripts/Class_07_2_Static.cs
@@ -1,4 +1,5 @@
 ﻿using UnityEngine;
+using UnityEngine.SceneManagement;
 
 namespace OLIVER
 {
@@ -7,18 +8,59 @@ namespace OLIVER
     /// </summary>
     public class Class_07_2_Static : MonoBehaviour
     {
+        #region 參數與屬性
+        // 成員
+        // 變數、屬性、方法
+
         // 非靜態變數
         public int inventoryWater = 10;
         // 靜態變數 : 修飾詞後面添加關鍵字 static
         // 靜態變數不顯示在屬性面板上
         public static int inventoryProp = 20;
 
+        // 非靜態屬性
+        public string skillMain => "火球術";
+        // 靜態屬性
+        public static string skillSecond => "治癒術"; 
+        #endregion
+
         private void Awake()
         {
-            inventoryWater = 7;
-            inventoryProp = 15;
+            // inventoryWater = 7;
+            // inventoryProp = 15;
             Debug.Log($"<color=#f31>藥水 : {inventoryWater}</color>");
             Debug.Log($"<color=#f31>道具 : {inventoryProp}</color>");
+        }
+
+        public void Punch()
+        {
+            Debug.Log("<color=#7f7>使用拳擊</color>");
+        }
+
+        public static void Kick()
+        {
+            Debug.Log("<color=#7f7>使用踢擊</color>");
+        }
+
+        private void Start()
+        {
+            // 在 Unity 內的差異
+            // 場景切換時
+            // 非靜態成員會被釋放 (還原為預設值)
+            inventoryWater++;         // 藥水加一
+            Debug.Log($"<color=#78f>藥水 : {inventoryWater}</color>");
+            inventoryProp++;          // 道具加一
+            Debug.Log($"<color=#78f>道具 : {inventoryProp}</color>");
+        }
+
+        private void Update()
+        {
+            // 在Game 畫面按下英文 W 會重新載入場景 (切換為英文輸入法)
+            // 如果按下鍵盤 W (左邊鍵盤)
+            if (Input.GetKeyDown(KeyCode.W))
+            {
+                SceneManager.LoadScene("課程07_屬性與靜態");
+            }
         }
     }
 }
