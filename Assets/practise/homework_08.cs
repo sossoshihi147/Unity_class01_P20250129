2bb7f39d3a629d95b60c7bfdc94c9d66c4e3c60e
sossoshihi147 <sossoshihi147@gmail.com>
2025-02-13 14:08:34 +0800

    Update 作業8_類別與建構子

---
+++ Assets/practise/homework_08.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+
+namespace OLIVER
+{
+    /// <summary>
+    /// 練習_類別及建構子
+    /// </summary>
+    public class homework_08 : MonoBehaviour
+    {
+        private void Start()
+        {
+            homework_08_Boss boss1 = new homework_08_Boss("小粉紅", "玻璃心", 1);
+            homework_08_Boss boss2 = new homework_08_Boss("西索", "伸縮自在的愛", 999);
+
+            boss1.Talk();
+            boss2.Talk();
+        }
+
+    }
+}
---
+++ Assets/practise/homework_08_Boss.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+
+namespace OLIVER
+{
+    public struct homework_08_Boss
+    {
+        private string bossName;
+        private string uniqueSkill;
+        private int bossHp;
+
+        public homework_08_Boss(string _name, string _skill, int _hp)
+        {
+            bossName = _name;
+            uniqueSkill = _skill;
+            bossHp = _hp;
+        }
+
+        public void Talk()
+        {
+            Debug.Log($"<color=#ff0>!!!{bossName}出現了，血量 :{bossHp} ，絕招是 :{uniqueSkill}!!!!</color>");
+        }
+
+    }
+
+}
+
