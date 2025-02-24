8eab7b9fc5118aca7d5dc39e9cfaa81ded78202c
sossoshihi147 <sossoshihi147@gmail.com>
2025-02-24 13:37:11 +0800

    Update 作業資料轉換

---
+++ Assets/practise/homework_09.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+
+namespace OLIVER
+{
+    public class homework_09 : MonoBehaviour
+    {
+        private void Awake()
+        {
+            float maxFloat = -999.321f;
+            sbyte maxFloarToSbyte = 0;
+            maxFloarToSbyte = (sbyte)maxFloat;
+            LogSystem.LogWithColor(maxFloarToSbyte, "#3f0");
+            LogSystem.LogWithColor(maxFloarToSbyte.GetType(), "#3f0");
+        }
+    }
+}
