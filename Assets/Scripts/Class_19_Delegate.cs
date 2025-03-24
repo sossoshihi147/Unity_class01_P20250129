0539beba2ae8e633f4f015d1a353aaef736291ba
sossoshihi147 <sossoshihi147@gmail.com>
2025-03-24 19:48:27 +0800

    Update 委派以及Lambda

--- Assets/Scripts/Class_19_Delegate.cs
+++ Assets/Scripts/Class_19_Delegate.cs
@@ -1,4 +1,5 @@
-﻿using UnityEngine;
+﻿using System;
+using UnityEngine;
 
 namespace OLIVER.Class_19
 {
@@ -83,9 +84,27 @@ namespace OLIVER.Class_19
         private DelegateCombine<float> delegateCombineFloat;
         private DelegateCombine<int> delegateCombineInt;
 
+        // 三種Unity官方已定義的委派
+        // Func
+        // 可以存放有傳回並且有 0 個以上參數的方法
+        // 宣告一個委派，有一個參數 float ，並且傳回值為 float
+        // <參數，傳回值>
+        private Func<float, float> funCalc;
+
+        // Action
+        // 可以存放無傳回並且有 0 個以上參數的方法
+        // 宣告一個委派，沒有參數也沒有傳回值
+        private Action actionMethod;
+
+        // Predicate
+        // 可以存放有布林值傳回並且有 1 個參數的方法
+        // 宣告一個委派，有一個參數 float ，並且傳回值為 bool
+        private Predicate<float> predicate;
+
         // 4. 呼叫委派
         private void Start()
         {
+
             #region 基本委派
             delegateMethod = Test;          // 將方法 Test 存放到變數 delegateMethod 內
             delegateMethod += Talk;         // 多播委派 : 一個委派存放多個方法，相同簽章
@@ -94,6 +113,7 @@ namespace OLIVER.Class_19
             delegateMethod();               // 呼叫委派 
             #endregion
 
+            #region 多播委派與泛型委派
             calculate += Add;
             calculate += Sub;
             calculate += Mul;
@@ -104,13 +124,52 @@ namespace OLIVER.Class_19
             delegateCombineFloat(3.3f);
             delegateCombineInt = Combine<int>;
             delegateCombineInt(797);
+            #endregion
+
+            #region 匿名方法
+            // 匿名的方法
+            // delegate (參數) { 陳述式 }
+            DelegateMethod anonymousMethod = delegate () { };
+            Calculate anonymousCalc = delegate (float a, float b) { return a * b; };
+            // 簡寫方法
+            DelegateMethod anonymousMethod2 = () => { };
+            Calculate anonymousCalc2 = (a, b) => { return a * b; };
+
+            CalcuteNumber(Add, 3, 7);
+            CalcuteNumber(anonymousCalc, 3, 7);
+            CalcuteNumber(anonymousCalc2, 3, 7);
+
+            // 在參數上使用匿名方法
+            CalcuteNumber(delegate (float a, float b) { return a / b; }, 9, 3);
+            CalcuteNumber((a, b) => { return a / b; }, 9, 2);
+            #endregion
+
+            funCalc = delegate (float x) { return x * 10; };
+            LogSystem.LogWithColor($"Func 委派：{funCalc(3.5f)}", "#f33");
+
+            actionMethod = delegate () { LogSystem.LogWithColor($"Action 委派", "#f33"); };
+            actionMethod();
+
+            predicate = delegate(float x) { return x > 0; };
+            LogSystem.LogWithColor($"Predicate 委派： 7 是否大於零 -> {predicate(7)}", "#f33");
+
+            // Lambda 運算子 =>
+            // (參數) => { 陳述式 }
+            Action action = () => { LogSystem.LogWithColor($"Lambda 練習", "#ff3"); };
+            action();
+
+            Func<int, string> func = (x) => { return $"Lambda 練習，數字：{x}"; };
+            LogSystem.LogWithColor(func(77), "#ff3");
+
+            Predicate<string> predicateTest = (x) => { return x.Length > 0; };
+            LogSystem.LogWithColor(predicateTest("nice"), "#ff3");
         }
 
         // 委派 : 將方法當作參數
         private void CalcuteNumber(Calculate calculate, float a, float b)
         {
             var result = calculate(a, b);
-            LogSystem.LogWithColor(result, "#ff3");
+            LogSystem.LogWithColor(result, "#f07");
         }
     }
 }
